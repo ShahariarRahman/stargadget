209aa43d18be27144112db58bb1c892ba4a6aec3
ShahariarRahman <shahariarrahman98@gmail.com>
2023-09-24 08:06:45 +0600

    Featured Category Icons

--- src/utils/images.js
+++ src/utils/images.js
@@ -5,6 +5,31 @@ import printerOffer from "@/assets/images/home-banner/platinum-printer-offer.png
 import expressDelivery from "@/assets/images/home-banner/express-delivery.png";
 import budgetDesktop from "@/assets/images/home-banner/budget-desktop.png";
 
+import cpu from "@/assets/icons/cpu.png";
+import motherboard from "@/assets/icons/motherboard.png";
+import ram from "@/assets/icons/ram.png";
+import powerSupply from "@/assets/icons/power-supply.png";
+import storageDevice from "@/assets/icons/storage-device.png";
+import monitor from "@/assets/icons/monitor.png";
+import gpu from "@/assets/icons/gpu.png";
+import mouse from "@/assets/icons/mouse.png";
+import keyboard from "@/assets/icons/keyboard.png";
+import drone from "@/assets/icons/drone.png";
+import gimbal from "@/assets/icons/gimbal.png";
+import batteryForLaptop from "@/assets/icons/battery-for-laptop.png";
+import tv from "@/assets/icons/tv.png";
+import mobilePhone from "@/assets/icons/mobile-phone.png";
+import powerStation from "@/assets/icons/power-station.png";
+import vr from "@/assets/icons/vr.png";
+import smartWatch from "@/assets/icons/smart-watch.png";
+import actionCamera from "@/assets/icons/action-camera.png";
+import printer from "@/assets/icons/printer.png";
+import headphone from "@/assets/icons/headphone.png";
+import earbuds from "@/assets/icons/earbuds.png";
+import bluetoothSpeakers from "@/assets/icons/bluetooth-speakers.png";
+import ccCamera from "@/assets/icons/cc-camera.png";
+import gamingConsole from "@/assets/icons/gaming-console.png";
+
 export const Images = {
   banner: {
     freeDelivery,
@@ -13,5 +38,31 @@ export const Images = {
     printerOffer,
     expressDelivery,
     budgetDesktop,
+  },
+  category: {
+    cpu,
+    motherboard,
+    ram,
+    powerSupply,
+    storageDevice,
+    monitor,
+    gpu,
+    mouse,
+    keyboard,
+    drone,
+    gimbal,
+    batteryForLaptop,
+    tv,
+    mobilePhone,
+    powerStation,
+    vr,
+    smartWatch,
+    actionCamera,
+    printer,
+    headphone,
+    earbuds,
+    bluetoothSpeakers,
+    ccCamera,
+    gamingConsole,
   },
 };
