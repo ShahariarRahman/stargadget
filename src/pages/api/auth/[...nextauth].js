02ff238800556189718a26a49fac952f54c2e780
ShahariarRahman <shahariarrahman98@gmail.com>
2023-11-13 19:50:35 +0600

    Next Auth: configuration

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // maybe to avoid error in private routes
+  pages: {
+    signIn: "/account/login", // if privater-route => navigate to /account/login
+  },
+};
+
+export default NextAuth(authOptions);
